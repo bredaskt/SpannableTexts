fa7ec2860205115ed5b763ac7dd404a96c081864
= <=>
2020-02-27 20:31:37 -0300

    Adjusts

--- app/src/main/java/com/bbreda/spannabletexts/MainActivity.kt
+++ app/src/main/java/com/bbreda/spannabletexts/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.bbreda.spannabletexts
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val myTextBoldSpannable = "Hello World !"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSpannableTexts()
+    }
 
-        tvMyText.text = setBoldSpannable(myTextBoldSpannable)
+    private fun setSpannableTexts() {
+        val myTextBoldSpannable = "This is a text bold spannable !"
+        val myTextColorSpannable = "This is a text color spannable !"
 
+        tvMyTextBold.text = setBoldSpannable(myTextBoldSpannable)
+        tvMyTextSpannable.text = setColorSpannable(myTextColorSpannable)
     }
 
     private fun setBoldSpannable(myText: String): SpannableString {
@@ -28,7 +34,17 @@ class MainActivity : AppCompatActivity() {
             myText.length / 2,
             Spannable.SPAN_INCLUSIVE_INCLUSIVE
         )
-
         return spannableContent
     }
+
+    private fun setColorSpannable(myText: String): SpannableString {
+        val spannableContent = SpannableString(myText)
+        spannableContent.setSpan(
+            ForegroundColorSpan(Color.RED),
+            8, 12,
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE)
+        return spannableContent
+    }
+
+
 }
